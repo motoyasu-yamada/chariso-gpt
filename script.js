c570dd6e802dd6a4aa279d3193582ada7456a49a
Motoyasu-Yamada <motoyasu.yamada@gmail.com>
2023-03-18 10:30:21 +0900

    チャリがジャンプしてくれないです

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ document.addEventListener('keydown', (e) => {
 function jump() {
   if (player.classList.contains('jump')) return;
   player.classList.add('jump');
+  player.style.bottom = '50px'; // この行を追加
 
   setTimeout(() => {
     player.classList.remove('jump');
+    player.style.bottom = '10px'; // この行を追加
   }, 500);
 }
 
